a16e761902dc3bf3e8564cb00251ed7dbf7b5ff0
harsh52 <hkumar8897@gmail.com>
2020-07-18 19:50:20 +0530

    PermCheck{codility}

---
+++ Algo_practice/PermCheck.py
@@ -0,0 +1,68 @@
+
+'''
+
+
+A non-empty array A consisting of N integers is given.
+
+A permutation is a sequence containing each element from 1 to N once, and only once.
+
+For example, array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+
+is a permutation, but array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+
+is not a permutation, because value 2 is missing.
+
+The goal is to check whether array A is a permutation.
+
+Write a function:
+
+    def solution(A)
+
+that, given an array A, returns 1 if array A is a permutation and 0 if it is not.
+
+For example, given array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+    A[3] = 2
+
+the function should return 1.
+
+Given array A such that:
+    A[0] = 4
+    A[1] = 1
+    A[2] = 3
+
+the function should return 0.
+
+Write an efficient algorithm for the following assumptions:
+
+        N is an integer within the range [1..100,000];
+        each element of array A is an integer within the range [1..1,000,000,000].
+
+
+'''
+
+
+def solution(A):
+	A.sort()
+	set1 = set(A) 	#O(nlogn)
+	j = 0
+	for i in set1:
+		j += 1
+		if(i!=j):
+			#print('0')
+			return 0
+	#print('1')
+	return(1)
+
+
+A = [4,1,3,]
+solution(A)
