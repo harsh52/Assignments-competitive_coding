eb8ce4eef29650b4afdc76634d5c9d410fbd076a
harsh52 <hkumar8897@gmail.com>
2020-07-19 21:11:51 +0530

    supernatural{hackerearth}

--- Algo_practice/supernatural.py
+++ Algo_practice/supernatural.py
@@ -1,3 +1,21 @@
+'''
+
+You are given a number n.
+
+A supernatural number is a number whose product of digits is equal to n, and in this number there is no digit 1.
+
+Count the number of supernatural numbers for a given n.
+
+Input
+
+Contains a single integer n, 1 <= n <= 100.
+
+Output
+
+Print the number of supernatural numbers.
+
+'''
+
 def isprime(n):
     i = 2
     while(i*i<=n):
