fdd79de6edf92e91c571b6e9c5a6991cd9b0673f
harsh52 <hkumar8897@gmail.com>
2020-07-25 13:50:48 +0530

    notificatio-icon

---
+++ R/tut_3{stat_notification_icon}/ui.r
@@ -0,0 +1,31 @@
+library(shinydashboard)
+library(shiny)
+shinyUI(
+  dashboardPage(
+    dashboardHeader(title = "THis is header",
+                      dropdownMenu(type = "message",
+                                    messageItem(from = "Finance update",message="We are broke"),
+                                    messageItem(from = "Sales update", message="I need a job", icon = icon("bar-chart"))
+                                   )
+                    ),
+    dashboardSidebar(
+    sliderInput("bins","Number of Breaks", 1,100,50),
+    sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuSubItem("Dashboard Finance", tabName = "finance"),
+      menuSubItem("Dashboard Sales",tabName = "sales"),
+    menuItem("Details Analysis"),
+    menuItem("Raw Data")
+    )),
+   dashboardBody(
+     tabItems(
+       tabItem(tabName = "dashboard",
+               fluidRow(
+                 box(plotOutput("histogram")))
+     ),
+      tabItem(tabName = "finance",h1("F! board")),
+      tabItem(tabName = "sales", h2("Sales dashboard"))
+     )
+   )
+)
+)
