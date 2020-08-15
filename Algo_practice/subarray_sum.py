8d9fe6182eccd3b13af8791273624abce838a34e
harsh52 <hkumar8897@gmail.com>
2020-08-15 19:56:47 +0530

    subarray

--- Algo_practice/subarray_sum.py
+++ Algo_practice/subarray_sum.py
@@ -1,30 +1,18 @@
 def solution(A,SUM1):
-	curr_sum = 0
+	curr_sum = A[0]
 	sub_array = []
 	j = 0
-	for i in range(len(A)+1):
-		
+	for i in range(len(A)):
+		while(curr_sum>SUM1 and j < i):
+			curr_sum = curr_sum - A[j]
+			j+=1
+		if(curr_sum == SUM1):
+				print(A[j:i-1])
+				print(j," ",i-1)
 		if(curr_sum<SUM1):
 			curr_sum = curr_sum + A[i]
-			print("1,",curr_sum)
-			if(curr_sum==SUM1):
-				sub_array=A[j:i]
-				#print("gjn")
-				print(sub_array)
-		elif(curr_sum>SUM1):
-			curr_sum = curr_sum - A[j]
-			print("2,",curr_sum)
-			j += 1
-			if(curr_sum==SUM1):
-				sub_array=A[j:i]
-				#print("gjn")
-				print(sub_array)
-		#elif(curr_sum==SUM1):
-		#	sub_array=A[j:i]
-			#print("gjn")
-			#print(sub_array)
 
 
-A = [1, 4, 20, 3, 10, 5]
-SUM1 = 33
-solution(A,33)
+A = [15, 2, 4, 8, 9, 5, 10, 23]
+SUM1 = 23
+solution(A,SUM1)
