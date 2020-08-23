df377ca6f15b21428fe709c36fab1b5269a5cba6
harsh52 <hkumar8897@gmail.com>
2020-08-23 13:35:58 +0530

    Leet_code:Two_sum

---
+++ Algo_practice/Twosum.py
@@ -0,0 +1,35 @@
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+'''
+
+def solution(A,target):
+	hash1 = dict()
+	for i in range(len(A)):
+		hash1[A[i]] = i
+	print(hash1)
+	for key,value in hash1.items():
+		index1 = hash1[key]
+		key1 = target - key
+		index2 = hash1.get(key1)
+		if(index2!= None):
+			out_list = [index1,index2]
+			print(out_list)
+			return(out_list)
+			break
+
+
+A = [3,2,4]
+
+
+target = 6
+
+solution(A,target)
