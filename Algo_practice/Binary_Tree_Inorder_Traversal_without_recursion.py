c0f865f88ba6eeb2245a84b6f21678e8c8f7b30c
harsh52 <hkumar8897@gmail.com>
2021-02-22 19:18:13 +0530

    input modified and leet code

---
+++ Algo_practice/Binary_Tree_Inorder_Traversal_without_recursion.py
@@ -0,0 +1,28 @@
+'''Given the root of a binary tree, return the inorder traversal of its nodes' values.'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        q = collections.deque()
+        while(1):
+            while(root != None):
+                q.append(root)
+                root = root.left
+            #if(q == None):
+             #   break
+            if(q):
+                # stack is not empty
+                #when we use pop() for stack (LIFO)
+                #popleft() for queue (FIFO)
+                #root = q.popleft() #FIFO (Queue)
+                root = q.pop()#LIFO(stack)
+                print(root.val)
+                root = root.right
+            else:
+                #stack is empty
+                break
